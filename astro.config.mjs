5c7cd389a3c674524503acebf5b7c0ad49f77d3c
Pat Johnson <>
2024-10-08 14:45:56 -0400

    Trying to fix deploy issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://the-compass-group.github.io',
-  base: '/portfolio',
+  base: '/',
   integrations: [tailwind()]
 });
